315aec8f8d0b0a769a1cef06870ad46ed14b1a97
Bingjie Tang <bingjietang718@gmail.com>
2025-04-10 21:09:34 -0700

    remove rubric and resource component

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
 home_page = st.Page("home.py", title="Home")
-rubric_page = st.Page("rubric.py", title="Rubrics")
-resources_page = st.Page("resources.py", title="Resources")
 deploy_page = st.Page("deploy.py", title="Deployment Instructions")
 
-pg = st.navigation([home_page, rubric_page, resources_page, deploy_page])
+pg = st.navigation([home_page, deploy_page])
 st.set_page_config(page_title="DSCI 554 Final Project")
 pg.run()
 
